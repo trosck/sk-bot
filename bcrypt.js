08813c4f7b479b418e2a3609d9760689357f4b60
Sergey Belov <kort.dll@gmail.com>
2025-12-19 21:50:08 +0300

    remove commandsa

--- bcrypt.js
+++ bcrypt.js
@@ -1,6 +1,6 @@
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
-// console.log(await bcrypt.hash("psw", 10));
+console.log(await bcrypt.hash("", 10));
 
-import crypto from "crypto";
-console.log(crypto.randomBytes(64).toString("hex"));
+// import crypto from "crypto";
+// console.log(crypto.randomBytes(64).toString("hex"));
--- src/interactions/index.ts
+++ src/interactions/index.ts
@@ -8,29 +8,27 @@ import { logger } from "../logger.js";
 const defineCommand = <T extends ApplicationCommandOption>(cmd: T) => cmd;
 
 export const DiscordCommandList = [
-  defineCommand({
-    name: "test",
-    description: "Basic commandowez",
-    type: 1,
-    integration_types: [0, 1],
-    contexts: [0, 1, 2],
-  } as const),
-
-  defineCommand({
-    name: "test2",
-    description: "Basic commandi",
-    type: 1,
-    integration_types: [0, 1],
-    contexts: [0, 1, 2],
-  } as const),
-
-  defineCommand({
-    name: "stat",
-    description: "Show me my stats",
-    type: 1,
-    integration_types: [0, 1],
-    contexts: [0, 1, 2],
-  } as const),
+  // defineCommand({
+  //   name: "test",
+  //   description: "Basic commandowez",
+  //   type: 1,
+  //   integration_types: [0, 1],
+  //   contexts: [0, 1, 2],
+  // } as const),
+  // defineCommand({
+  //   name: "test2",
+  //   description: "Basic commandi",
+  //   type: 1,
+  //   integration_types: [0, 1],
+  //   contexts: [0, 1, 2],
+  // } as const),
+  // defineCommand({
+  //   name: "stat",
+  //   description: "Show me my stats",
+  //   type: 1,
+  //   integration_types: [0, 1],
+  //   contexts: [0, 1, 2],
+  // } as const),
 ] as const;
 
 export type CommandName = (typeof DiscordCommandList)[number]["name"];

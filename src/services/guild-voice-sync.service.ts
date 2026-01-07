c3a510ef2b4dd5cf8ed7b521678557dae2d96cbc
Sergey Belov <kort.dll@gmail.com>
2026-01-07 14:48:43 +0300

    fix handleVoiceStateUpdate and startSession, add syncSessions

--- src/services/guild-voice-sync.service.ts
+++ src/services/guild-voice-sync.service.ts
@@ -1,7 +1,8 @@
-import { GuildChannel, GuildMember, User, VoiceState } from "discord.js";
+import { Guild, GuildChannel, GuildMember, User, VoiceState } from "discord.js";
 import { prisma } from "../prisma.js";
 import { UserXpReward } from "../RewardSystem/UserXpReward.js";
 import { logger } from "../logger.js";
+import { VoiceSession } from "../../generated/prisma/client.js";
 
 function isJoined(oldState: VoiceState, newState: VoiceState) {
   return !oldState.channelId && newState.channelId;
@@ -15,18 +16,18 @@ function isMuted(state: VoiceState) {
   return state.selfMute;
 }
 
-function isDeafened(state: VoiceState) {
-  return state.selfDeaf;
-}
-
 function isUnmuted(oldState: VoiceState, newState: VoiceState) {
   return oldState.selfMute && !newState.selfMute;
 }
 
 export class GuildVoiceSync {
-  static async startSession(member: GuildMember, channel: GuildChannel) {
-    const session = await prisma.voiceSession.create({
-      data: {
+  static async startSession(member: GuildMember) {
+    const session = await prisma.voiceSession.upsert({
+      where: {
+        user_id: member.user.id,
+      },
+      update: {},
+      create: {
         user_id: member.user.id,
       },
     });
@@ -61,19 +62,54 @@ export class GuildVoiceSync {
   }
 
   static async handleVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
-    if (isJoined(oldState, newState)) {
-      await this.startSession(newState.member!, newState.channel!);
-      logger.debug(`Started voice session for ${newState.member!.user.username} in ${newState.channel!.name}`);
-    }
-
-    if (isMuted(newState) || isDeafened(newState) || isLeft(oldState, newState)) {
+    if (isMuted(newState) || isLeft(oldState, newState)) {
       await this.endSession(newState.member!);
       logger.debug(`Ended voice session for ${newState.member!.user.username} in ${oldState.channel!.name}`);
+      return;
     }
 
-    if (isUnmuted(oldState, newState)) {
-      await this.startSession(newState.member!, newState.channel!);
-      logger.debug(`User ${newState.member!.user.username} unmuted in ${newState.channel!.name}, started voice session`);
+    if (isJoined(oldState, newState) || isUnmuted(oldState, newState)) {
+      await this.startSession(newState.member!);
+      logger.debug(`Started voice session for ${newState.member!.user.username} in ${newState.channel!.name}`);
+      return;
+    }
+  }
+
+  static async syncSessions(guild: Guild) {
+    const sessions = await prisma.voiceSession.findMany({});
+
+    const sessionsMap = new Map<string, VoiceSession>(sessions.map((session) => [session.user_id, session]));
+
+    const activeMembersMap = new Map<string, GuildMember>();
+
+    for (const state of guild.voiceStates.cache.values()) {
+      const member = state.member!;
+
+      if (!isMuted(state)) {
+        activeMembersMap.set(member.user.id, member);
+
+        const session = sessionsMap.get(member.user.id);
+        if (!session) {
+          await this.startSession(member);
+          logger.debug(`Started voice session for ${member.user.username} in ${state.channel!.name}`);
+          continue;
+        }
+      }
+    }
+
+    for (const session of sessions) {
+      const activeMember = activeMembersMap.get(session.user_id);
+      if (activeMember) {
+        continue;
+      }
+
+      const member = guild.members.cache.get(session.user_id);
+      if (!member) {
+        continue;
+      }
+
+      await this.endSession(member);
+      logger.debug(`Ended voice session for ${member.user.username}`);
     }
   }
 }

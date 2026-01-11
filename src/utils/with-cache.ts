83eb62770f89b0177d820c20c00e968ca46323a2
Sergey Belov <kort.dll@gmail.com>
2026-01-12 00:57:59 +0300

    add WithCache decorator

---
+++ src/utils/with-cache.ts
@@ -0,0 +1,25 @@
+import NodeCache from "@cacheable/node-cache";
+
+const cache = new NodeCache<any>({
+  stdTTL: "30m",
+});
+
+export function WithCache(key: string) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+      const cacheKey = key + JSON.stringify(args);
+
+      if (!cache.has(cacheKey)) {
+        const result = originalMethod.apply(this, args);
+
+        cache.set(cacheKey, result);
+
+        return result
+      }
+
+      return cache.get(cacheKey);
+    };
+  }
+}

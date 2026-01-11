69fa712611ed18cc747a07e5229b95cf3363908d
Sergey Belov <kort.dll@gmail.com>
2026-01-12 01:02:44 +0300

    add adminMiddleware, check roles in authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,8 @@ import type {
 import { isDev, JWT_SECRET_ACCESS } from "./config.js";
 import { logger } from "./logger.js";
 import jwt from "jsonwebtoken";
+import { GuildMemberSyncService } from "./services/guild-member-sync.service.js";
+import { AppConfigService } from "./services/app-config.service.js";
 
 export const logMiddleware: RequestHandler = (
   req: Request,
@@ -72,16 +74,50 @@ export const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
   res.status(status).json(data);
 };
 
-export const authMiddleware: RequestHandler = (req, res, next) => {
+export const authMiddleware: RequestHandler = async (req, res, next) => {
   const token = req.cookies?.access_token;
   if (!token) {
     return res.status(401).json({ error: "No access token 🫥" });
   }
 
+  let decoded: { id: string };
   try {
-    jwt.verify(token, JWT_SECRET_ACCESS);
-    return next();
+    decoded = jwt.verify(token, JWT_SECRET_ACCESS) as { id: string };
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
   }
+
+  const user = await GuildMemberSyncService.getUser(decoded.id);
+  if (!user) {
+    return res.status(401).json({ error: "User not found" });
+  }
+
+  const appConfig = await AppConfigService.getAppConfig();
+
+  const hasAccessRole = appConfig.access_roles.some((role) => user.roles.includes(role));
+  if (!hasAccessRole) {
+    return res.status(401).json({ error: "User does not have access role" });
+  }
+
+  return next();
 };
+
+export const adminMiddleware: RequestHandler = async (req, res, next) => {
+  const token = req.cookies?.access_token;
+  if (!token) {
+    return res.status(401).json({ error: "No access token 🫥" });
+  }
+
+  let decoded: { id: string };
+  try {
+    decoded = jwt.verify(token, JWT_SECRET_ACCESS) as { id: string };
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+
+  if (decoded.id !== "admin") {
+    return res.status(401).json({ error: "User is not admin" });
+  }
+
+  return next();
+}

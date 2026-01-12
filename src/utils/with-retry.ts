8ed31eec615dbf73b8a27e4bdf7049d8e807106c
Sergey Belov <kort.dll@gmail.com>
2026-01-12 03:27:15 +0300

    add sleep delay

--- src/utils/with-retry.ts
+++ src/utils/with-retry.ts
@@ -1,10 +1,16 @@
 import { logger } from "../logger.js";
+import { sleep } from "./sleep.js";
 
 export async function withRetry(cb: () => Promise<any>) {
   let attempt = 0;
   const retries = 3;
 
-  while (++attempt <= retries) {
+  while (attempt++ <= retries) {
+    if (attempt > 1) {
+      const delay = 1 << attempt;
+      await sleep(delay * 1000);
+    }
+
     try {
       await cb();
       return;
